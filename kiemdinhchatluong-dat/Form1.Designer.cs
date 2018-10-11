bdebae3b005b9eb7dca2655c8e2d920aed2d6b55
datne <dat.vt.15dh110348@outlook.com>
2018-10-11 08:51:11 +0700

    tao du an kiem dinh thuc hanh

---
+++ kiemdinhchatluong-dat/Form1.Designer.cs
@@ -0,0 +1,154 @@
+﻿namespace kiemdinhchatluong_dat
+{
+    partial class formCheck
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtDay = new System.Windows.Forms.TextBox();
+            this.txtMonth = new System.Windows.Forms.TextBox();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.btnClearData = new System.Windows.Forms.Button();
+            this.btnCheck = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(35, 45);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Day:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(35, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Month:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(35, 172);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(47, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Year:";
+            // 
+            // txtDay
+            // 
+            this.txtDay.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDay.Location = new System.Drawing.Point(105, 46);
+            this.txtDay.MaxLength = 2;
+            this.txtDay.Name = "txtDay";
+            this.txtDay.Size = new System.Drawing.Size(131, 24);
+            this.txtDay.TabIndex = 3;
+            this.txtDay.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            // 
+            // txtMonth
+            // 
+            this.txtMonth.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMonth.Location = new System.Drawing.Point(105, 107);
+            this.txtMonth.MaxLength = 2;
+            this.txtMonth.Name = "txtMonth";
+            this.txtMonth.Size = new System.Drawing.Size(131, 24);
+            this.txtMonth.TabIndex = 4;
+            this.txtMonth.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            // 
+            // txtYear
+            // 
+            this.txtYear.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtYear.Location = new System.Drawing.Point(105, 168);
+            this.txtYear.MaxLength = 4;
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(131, 24);
+            this.txtYear.TabIndex = 5;
+            this.txtYear.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtDay_KeyPress);
+            // 
+            // btnClearData
+            // 
+            this.btnClearData.Location = new System.Drawing.Point(52, 216);
+            this.btnClearData.Name = "btnClearData";
+            this.btnClearData.Size = new System.Drawing.Size(75, 23);
+            this.btnClearData.TabIndex = 6;
+            this.btnClearData.Text = "Clear";
+            this.btnClearData.UseVisualStyleBackColor = true;
+            this.btnClearData.Click += new System.EventHandler(this.btnClearData_Click);
+            // 
+            // btnCheck
+            // 
+            this.btnCheck.Location = new System.Drawing.Point(170, 216);
+            this.btnCheck.Name = "btnCheck";
+            this.btnCheck.Size = new System.Drawing.Size(75, 23);
+            this.btnCheck.TabIndex = 7;
+            this.btnCheck.Text = "Check";
+            this.btnCheck.UseVisualStyleBackColor = true;
+            this.btnCheck.Click += new System.EventHandler(this.btnCheck_Click);
+            // 
+            // formCheck
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 262);
+            this.Controls.Add(this.btnCheck);
+            this.Controls.Add(this.btnClearData);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.txtMonth);
+            this.Controls.Add(this.txtDay);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "formCheck";
+            this.Text = "KIỂM TRA NGÀY THÁNG";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.formCheck_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtDay;
+        private System.Windows.Forms.TextBox txtMonth;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Button btnClearData;
+        private System.Windows.Forms.Button btnCheck;
+    }
+}
+
---
+++ kiemdinhchatluong-dat/Form1.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace kiemdinhchatluong_dat
+{
+    public partial class formCheck : Form
+    {
+        public formCheck()
+        {
+            InitializeComponent();
+        }
+
+        private void btnClearData_Click(object sender, EventArgs e)
+        {
+            txtDay.Text = "";
+            txtMonth.Text = "";
+            txtYear.Text = "";
+        }
+
+        private void txtDay_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(!Char.IsDigit(e.KeyChar) && !Char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+
+        private void btnCheck_Click(object sender, EventArgs e)
+        {
+            if (!string.IsNullOrEmpty(txtDay.Text) && !string.IsNullOrEmpty(txtMonth.Text) && !string.IsNullOrEmpty(txtYear.Text))
+            {
+                if (this.checkValidate(txtDay.Text,txtMonth.Text,txtYear.Text))
+                {
+                    MessageBox.Show(this.checkDate(txtMonth.Text,txtYear.Text).ToString());
+                    
+                }
+                else { MessageBox.Show("Đầu vào không hợp lệ !"); }
+            }
+            else { MessageBox.Show("chua co chu nao"); }        
+        }
+
+        private bool checkValidate(string day,string month,string year)
+        {
+            int n;        
+            if((int.Parse(day) <= 31 && int.Parse(day) >= 1) && (int.Parse(month) <= 12 && int.Parse(month) >= 1))
+            {
+                return true;
+            }
+            else if(int.TryParse(year, out n))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        private int checkDate(string monthInput, string yearInput)
+        {
+            int year = int.Parse(yearInput);
+            int month = int.Parse(monthInput);
+            int[] date31 = { 1, 3, 5, 7, 8, 10, 12 };
+            int[] date30 = { 4, 6, 9, 11 };          
+            if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0)
+            {
+                //nhuan
+                if (!date31.Contains(month) && !date30.Contains(month))
+                {
+                    // la thang 2 cua nam nhuan
+                    return 29;
+                }
+                else
+                {
+                    if (date31.Contains(month)) { return 31; }
+                    else { return 30; }
+                }          
+            }
+            else {
+                if (!date31.Contains(month) && !date30.Contains(month))
+                {
+                    // la thang 2 cua nam khong nhuan
+                    return 28;
+                }
+                else
+                {
+                    if (date31.Contains(month)) { return 31; }
+                    else { return 30; }
+                }
+            }        
+
+        }
+
+        private void formCheck_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if (MessageBox.Show("Exit or no?",
+                       "Confirm",
+                        MessageBoxButtons.YesNo,
+                        MessageBoxIcon.Information) == DialogResult.No)
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
